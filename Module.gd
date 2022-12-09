1878cf26b21638f3ac1bc56f1cf39a3668f2cf95
PfandBoss <68470553+PfandBoss@users.noreply.github.com>
2022-12-09 23:39:45 +0100

    Modul Oberklasse geadded

---
+++ Module.gd
@@ -0,0 +1,42 @@
+extends Node
+
+#-----------Parameters----------------'
+var currentStashValue  = 0 : set = _set_currentStashValue, get = _get_currentStashValue
+
+var maxStashValue  = 0 : set = _set_maxStashValue, get = _get_maxStashValue
+
+var currentState = null : set = _set_state, get = _get_state
+
+var level = 0 : set = _set_level, get = _get_level
+
+
+enum MODULE_TYPE {CANNON, STEERING, ENGINE, STORAGE}
+
+
+
+#------------Methods-------------#
+
+
+func interact():
+	pass
+
+
+
+
+#-----------Setter and Getter---------------#
+func _set_currentStashValue(newValue):
+	currentStashValue = newValue
+func _get_currentStashValue():
+	return currentStashValue
+func _set_maxStashValue(newValue):
+	currentStashValue = newValue
+func _get_maxStashValue():
+	return currentStashValue
+func _set_level(newValue):
+	level = newValue
+func _get_level():
+	return level
+func _set_state(newValue):
+	currentState = newValue
+func _get_state():
+	return currentState
