7c96139422538c4b6868abaa69c1ac8b542cd4d2
Lukas Moungos <lukas.moungos@tum.de>
2022-12-10 23:30:49 +0100

    merge

--- scripts/Logic/InfiniteScroller.gd
+++ scripts/Logic/InfiniteScroller.gd
@@ -1,11 +1,28 @@
 extends Node3D
 
-@onready var rails
-# Called when the node enters the scene tree for the first time.
+@onready var rails = preload("res://Assets/Model/Objects/rail_tracks.glb")
+# Called when the node enters the scene tree for the first time
+var offset = 0
 func _ready():
-	pass # Replace with function body.
+	var timer = Timer.new()
+	self.add_child(timer)
+	timer.timeout.connect(create_rail)
+	timer.set_wait_time(0.2)
+	timer.start()
 
+func create_rail():
+	var obj = rails.instantiate()
+	obj.rotation.y = deg_to_rad(90)
+	obj.position = Vector3(0,0,13)
+	offset -= 13
+	add_child(obj)
+	
+	var timer = Timer.new()
+	obj.add_child(timer)
+# Connect the timer to make it call "queue_free" after two seconds
+	timer.timeout.connect(obj.queue_free)
+	timer.set_wait_time(2)
+	timer.start()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	position.z += delta * 40
