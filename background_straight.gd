5f02f31566a22964a395c71a583c58c36f414be1
PfandBoss <68470553+PfandBoss@users.noreply.github.com>
2022-12-10 10:32:39 +0100

    push push

--- background_straight.gd
+++ background_straight.gd
@@ -1,13 +1,11 @@
 extends Node3D
 
-var background
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	background = preload("res://background_straight.tscn").instantiate()
-	add_child(background)
-	print(background.get_position())
-
+	var tween = create_tween()
+	tween.tween_property(self,"position",Vector3(-8,0,40),5)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	print(background.get_position())
+	pass
