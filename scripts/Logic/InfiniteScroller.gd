ac725c194f0dfa3d97ebb364db10762002ae8fd3
Lukas Moungos <lukas.moungos@tum.de>
2022-12-11 00:42:21 +0100

    infinite scroll

--- scripts/Logic/InfiniteScroller.gd
+++ scripts/Logic/InfiniteScroller.gd
@@ -1,9 +1,17 @@
 extends Node3D
 
-@onready var rails = preload("res://Assets/Model/Objects/rail_tracks.glb")
+@onready var rails = preload("res://Nodes/rail_tracks.tscn")
+@onready var container = $Container
 # Called when the node enters the scene tree for the first time
-var offset = 0
+var offset = -13
+var last_obj
 func _ready():
+	var obj = rails.instantiate() as Node3D
+	#obj.position = position
+	add_child(obj)
+	obj.position = Vector3.ZERO
+	last_obj = obj
+	
 	var timer = Timer.new()
 	self.add_child(timer)
 	timer.timeout.connect(create_rail)
@@ -12,17 +20,7 @@ func _ready():
 
 func create_rail():
 	var obj = rails.instantiate()
-	obj.rotation.y = deg_to_rad(90)
-	obj.position = Vector3(0,0,13)
-	offset -= 13
+	#obj.position = position
+	obj.position.z = last_obj.position.z + offset
 	add_child(obj)
-	
-	var timer = Timer.new()
-	obj.add_child(timer)
-# Connect the timer to make it call "queue_free" after two seconds
-	timer.timeout.connect(obj.queue_free)
-	timer.set_wait_time(2)
-	timer.start()
-
-func _process(delta):
-	position.z += delta * 40
+	last_obj = obj
