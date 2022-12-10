ffe9b2f047b1d18da92f6f8c54f24beff59c6c68
PfandBoss <68470553+PfandBoss@users.noreply.github.com>
2022-12-10 09:49:40 +0100

    Adds

---
+++ Nodes/Backgroundspawner.gd
@@ -0,0 +1,12 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var background = preload("res://Background_straight.tscn").instantiate()
+	add_child(background)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
