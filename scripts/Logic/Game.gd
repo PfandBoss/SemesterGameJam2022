f43307ac675f3f7404c2c4f72ba4f15c47626753
PfandBoss <68470553+PfandBoss@users.noreply.github.com>
2022-12-10 04:11:58 +0100

    e

--- scripts/Logic/Game.gd
+++ scripts/Logic/Game.gd
@@ -6,8 +6,8 @@ signal shoot
 
 var t = 0.0
 @onready var tween = create_tween() as Tween
-@onready var p1 = $Player1Path/PathFollow3D
-@onready var p2 = $Player2Path/PathFollow3D
+@export var p1 : PathFollow3D
+@export var p2 : PathFollow3D
 
 
 func _ready():
@@ -16,8 +16,8 @@ func _ready():
 func start_tween():
 	tween.set_trans(Tween.TRANS_LINEAR)	
 	tween.set_parallel(true)
-	tween.tween_property(p1, "progress_ratio", 1, 5)
-	tween.tween_property(p2, "progress_ratio", -1, 5)
+	tween.tween_property(p1, "progress_ratio", 1, 20)
+	tween.tween_property(p2, "progress_ratio", -1, 20)
 	
 func _process(delta):
 	if round(p1.position.distance_to(p2.position)) == 9:
