d6665c4995650c69111ab54fbd1255713a4b9ffe
Arthur <ge65hik@mytum.de>
2022-12-11 08:57:30 +0100

    Started Functionality of 3DUI

---
+++ scripts/3DUI/dreieck.gd
@@ -0,0 +1,7 @@
+extends Node3D
+
+func _on_area_3d_input_event(camera, event, position, normal, shape_idx):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed == true:
+			print("Start Game")
+	
