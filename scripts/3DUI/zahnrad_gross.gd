82fd7a610dfd13a353ca5e8d5a6d6abbe41770f2
Arthur <ge65hik@mytum.de>
2022-12-11 09:14:49 +0100

    hitbox for gear and papierrolle

--- scripts/3DUI/zahnrad_gross.gd
+++ scripts/3DUI/zahnrad_gross.gd
@@ -1,8 +1,10 @@
 extends Node3D
 
+
 func _on_area_3d_input_event(camera, event, position, normal, shape_idx):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed == true:
-			print("Start Game")
+			print("Settings")
+			#TODO: Settings:
 			get_tree().change_scene_to_file("res://Nodes/game.tscn")
 	
