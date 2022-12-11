14490c71e45e74f3dd61654f79d424dc7f402e34
INoro <okay.uenal@tum.de>
2022-12-11 01:50:33 +0100

    Leo - GUI WIP

--- menu/Speedometer_player1.gd
+++ menu/Speedometer_player1.gd
@@ -1,7 +1,6 @@
 extends CanvasLayer
 
 var currentSpeed
-
 var maxSpeed = float(TrainEngine.maxSpeed)
 var minSpeed = 0.0
 
@@ -9,22 +8,20 @@ var deg
 var rad
 
 @onready var root = self.get_parent()
-
+#acceptable rotations range from -35 to +35 degrees
 func _ready():
 	#get Game Node reference
 	while not root is Game:
 		root = root.get_parent()
 	
-	print(root.name)
-	print(maxSpeed)
+	print("maxSpeed =", maxSpeed)
 		
-#acceptable rotations range from -35 to +35 degrees
 func _process(delta):
 	
 	currentSpeed = root.p1_train.current_speed
-	deg = ((70.0/maxSpeed)* currentSpeed) - 35.0
-	rad = (deg * 3.14)/180.0
+	print("currentSpeed", currentSpeed)
+	deg = currentSpeed * (70.0/maxSpeed) - 35.0
+	print(deg)
+	rad = (deg * 3.14) / 180
 	self.get_node("Pfeil").rotation = rad
 
-func round_to_digit(num, digit):
-	return round(num * pow(10.0, digit)) / pow(10.0, digit)
---
+++ menu/stats_player_1.gd
@@ -0,0 +1,5 @@
+extends CanvasLayer
+
+func _ready():
+	var hbox = self.get_node("Rahmen/Ressourcen/RessourcenBox")
+	hbox.add_spacer(true)
