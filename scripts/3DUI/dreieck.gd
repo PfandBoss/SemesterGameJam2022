27106eeab6085773703bbb937781c7aa267226da
Arthur <ge65hik@mytum.de>
2022-12-11 10:11:14 +0100

    Functional 3D User Interface

--- scripts/3DUI/dreieck.gd
+++ scripts/3DUI/dreieck.gd
@@ -1,8 +1,13 @@
 extends Node3D
 
 func _on_area_3d_input_event(camera, event, position, normal, shape_idx):
+	self.scale = Vector3(1.5,1.5,1.5)
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed == true:
 			print("Start Game")
 			get_tree().change_scene_to_file("res://Nodes/game.tscn")
 	
+
+
+func _on_area_3d_mouse_exited():
+	self.scale = Vector3(1,1,1)
--- scripts/3DUI/zahnraeder.gd
+++ scripts/3DUI/zahnraeder.gd
@@ -2,9 +2,15 @@ extends Node3D
 
 
 func _on_area_3d_input_event(camera, event, position, normal, shape_idx):
+	print("scale")
+	self.scale = Vector3(1.2,1.2,1.2)
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed == true:
 			print("Settings")
 			#TODO: Settings:
 			get_tree().change_scene_to_file("res://Nodes/game.tscn")
 	
+
+
+func _on_area_3d_mouse_exited():
+	self.scale = Vector3(1,1,1)
