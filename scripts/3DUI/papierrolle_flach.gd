81f3a26e9cfd0bc4fafeeda633312b7cdd2768dc
Arthur <ge65hik@mytum.de>
2022-12-11 11:01:03 +0100

    Added Labels

--- scripts/3DUI/papierrolle_flach.gd
+++ scripts/3DUI/papierrolle_flach.gd
@@ -1,8 +1,11 @@
 extends Node3D
 
+func _ready():
+	self.get_node("tutorial").hide()
 
 func _on_area_3d_input_event(camera, event, position, normal, shape_idx):
 	self.scale = Vector3(1.5,1.5,1.5)
+	self.get_node("tutorial").show()
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed == true:
 			print("Credits/Spielbeschreibung")
@@ -13,3 +16,4 @@ func _on_area_3d_input_event(camera, event, position, normal, shape_idx):
 
 func _on_area_3d_mouse_exited():
 	self.scale = Vector3(1,1,1)
+	self.get_node("tutorial").hide()
